38c1f95ee8a77f90c84db20c2c36b4805d1ac6ce
VÕ THỊ YẾN NHI <yennhi20vl@gmail.com>
2023-02-07 20:56:34 +0700

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,28 @@
+// Slick slider
+$(document).ready(function () {
+  $(".quote-container").slick({
+    prevArrow:
+      "<button type='button' class='slick-prev pull-left'><i class='fal fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:
+      "<button type='button' class='slick-next pull-right'><i class='fal fa-angle-right' aria-hidden='true'></i></button>",
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
+});
+const menuToggle = document.querySelector(".header-toggle");
+const menuHeader = document.querySelector(".header-menu");
+const expandClass = "is-expand";
+menuToggle.addEventListener("click",function(){
+    menuHeader.classList.add(expandClass);
+});
+window.addEventListener("click",function(e){
+    if(!menuHeader.contains(e.target) && !e.target.matches(".header-toggle")){
+        menuHeader.classList.remove(expandClass);
+    }
+})
